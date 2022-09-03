6cdb178d2890a4b8d777401a0490bd49d4e152cf
nishant chandel <nishantxchandel@gmail.com>
2022-09-03 16:26:16 +0530

    hello

--- hackathon/settings.py
+++ hackathon/settings.py
@@ -27,9 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
@@ -37,6 +35,7 @@ CORS_ORIGIN_WHITELIST = [
 INSTALLED_APPS = [
     'website',
     'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -46,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
